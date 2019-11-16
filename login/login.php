8f5e7d86036b1918fde1f5ac5db372ae047275d5
rodinei <rodinei.lima97@gmail.com>
2019-11-16 14:26:49 -0300

    Adicionando pagina de login

--- login/login.php
+++ login/login.php
@@ -7,20 +7,20 @@
     <meta name="author" content="">
     <!--<link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico"> -->
 
-    <title>Portal ETEC</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/login.css" rel="stylesheet">
+    <title>Rei do Ovo</title>
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/login.css" rel="stylesheet">
 </head>
 
 <body class="text-center">
 <div class="wrapper fadeInDown">
     <div id="formContent">
         <h2 class="active">Entrar</h2>
-        <h2 class="inactive underlineHover">Portal ETEC </h2>
+        <h2 class="inactive underlineHover">Rei do Ovo</h2>
 
         <div class="fadeIn first">
 
-        <form method="POST" action="">
+        <form method="POST" action="../examples/dashboard.html">
             <input type="text" id="login" class="fadeIn second" name="login" placeholder="Usuário">
             <input type="password" id="password" class="fadeIn third" name="login" placeholder="Senha">
             <input type="submit" class="fadeIn fourth" value="Entrar">
